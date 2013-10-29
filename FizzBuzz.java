20ab9585e4b20ff7d91f17ad2ea24c14049cb650
Ryan Sheh <sheh_ryan@guest-wireless-upc-nat-206-117-88-010.usc.edu>
2013-10-29 10:18:25 -0700

    Merged files

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,17 +1,10 @@
 public class FizzBuzz {
-<<<<<<< HEAD
     public static void main(String[] args) {
         for(int i = 0; i < 100; i++) {
             if( i % 3 == 0)
                 System.out.println(i + ": " + "Fizz");
-        }
-    }
-=======
-	public static void main(String[] args) {
-		for(int i = 0; i < 100; i++) {
 			if( i % 5 == 0)
 				System.out.println("Buzz");
 		}
 	}
->>>>>>> 1019e3293bdc4cb57f824209f8b22a179b8df26e
 }
